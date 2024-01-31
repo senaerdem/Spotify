be6550cfb30a9dbcdf3aee03eb10b55293bc7e11
senaerdem <snnrrdm@gmail.com>
2024-01-31 19:23:30 +0300

    auth set up
    authentication is set and the user is allowed to log in

--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -9,7 +9,22 @@ import Foundation
 
 final class AuthManager {
     static let shared = AuthManager()
+    
+    struct Contants {
+        static let clientID = "7036c3ae7b0045fb8bb810a68804a104"
+        static let clientSecret = "fbb57d4e3cc0471fa70b96f51fb0fa21"
+        
+    }
+    
     private init() {}
+    
+    public var signInURL: URL? {
+        let scopes = "user-read-private"
+        let redirectURI = "https://www.apple.com"
+        let base = "https://accounts.spotify.com/authorize"
+        let string = "\(base)?response_type=code&client_id=\(Contants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        return URL(string: string)
+    }
     
     var isSignedIn:Bool {
         return false
@@ -29,5 +44,20 @@ final class AuthManager {
     
     private var shouldRefreshToken: Bool {
         return false
+    }
+    
+    public func exchangeCodeForToken(
+        code: String,
+        completion: @escaping ((Bool) -> Void)
+    ) {
+        // Get Token
+    }
+    
+    public func refreshAccessTooken() {
+        
+    }
+    
+    private func cacheToken() {
+        
     }
 }
